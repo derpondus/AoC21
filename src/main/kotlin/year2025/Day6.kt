355c53160604bb982091022507ebe52b1d98357e
derpondus <lasse.liening-ewert@t-online.de>
2025-12-05 12:55:33 +0100

    2025:6-7 templated

---
+++ src/main/kotlin/year2025/Day6.kt
@@ -0,0 +1,10 @@
+package year2025
+
+import kotlin.math.max
+import kotlin.math.min
+
+fun day6(inputLines: List<String>) {
+    val data = inputLines
+
+    println("[Part1] Number of Lines: ${data.size}")
+}
