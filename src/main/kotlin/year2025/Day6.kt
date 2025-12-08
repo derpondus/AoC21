3b4d0ae5ba7bf681e9cdc570650b3bee4d6bf17d
liamblind <liam.blind.24@web.de>
2025-12-08 15:57:40 +0100

    content: 2025:6-7, template: 2025:8-9

--- src/main/kotlin/year2025/Day6.kt
+++ src/main/kotlin/year2025/Day6.kt
@@ -1,10 +1,55 @@
 package year2025
 
-import kotlin.math.max
-import kotlin.math.min
+import kotlin.collections.map
+import kotlin.collections.mutableListOf
 
 fun day6(inputLines: List<String>) {
-    val data = inputLines
+    val operators = inputLines.last().trim().split("\\s+".toRegex())
+    val numbersP1 = inputLines.dropLast(1)
+        .fold(operators.map { mutableListOf<Long>() }) { out, line ->
+            line
+                .trim()
+                .split("\\s+".toRegex())
+                .map { it.toLong() }
+                .forEachIndexed { idx, num -> out[idx].add(num) }
+            out
+        }
 
-    println("[Part1] Number of Lines: ${data.size}")
+    val p1Sum = operators.zip(numbersP1)
+        .sumOf { (op, nums) ->
+        when (op) {
+            "+" -> nums.sum()
+            "*" -> nums.reduce { acc, num -> acc * num }
+            else -> throw IllegalStateException("Unknown op: $op")
+        }
+    }
+
+    println("[Part1] Sum of results: $p1Sum")
+    // 5595593539811 (correct)
+
+    val numbersP2 = inputLines.dropLast(1)
+        .foldIndexed(inputLines.first().map { CharArray(inputLines.size-1) }) { lineIdx, out, line ->
+            line.mapIndexed { charIdx, c -> out[charIdx][lineIdx] = c }
+            out
+        }
+        .map { it.joinToString("") }
+        .fold(mutableListOf(mutableListOf<Long>())) { out, numStr ->
+            if (numStr.isBlank()) out.add(mutableListOf())
+            else {
+                out.last().add(numStr.trim().toLong())
+            }
+            out
+        }
+
+    val p2Sum = operators.zip(numbersP2)
+        .sumOf { (op, nums) ->
+            when (op) {
+                "+" -> nums.sum()
+                "*" -> nums.reduce { acc, num -> acc * num }
+                else -> throw IllegalStateException("Unknown op: $op")
+            }
+        }
+
+    println("[Part2] Sum of results: $p2Sum")
+    // 10153315705125 (correct)
 }
--- src/main/kotlin/year2025/Day7.kt
+++ src/main/kotlin/year2025/Day7.kt
@@ -1,10 +1,25 @@
 package year2025
 
-import kotlin.math.max
-import kotlin.math.min
-
 fun day7(inputLines: List<String>) {
-    val data = inputLines
+    var p1Count = 0
+    val outBeams = inputLines.drop(1)
+        .fold(inputLines.first().map { if (it == 'S') 1L else 0L }.toMutableList()) { out, line ->
+            line.forEachIndexed { index, c ->
+                if (c == '^' && out[index] > 0) {
+                    out[index - 1] += out[index]
+                    out[index + 1] += out[index]
+                    out[index] = 0
+                    p1Count++
+                }
+            }
+            out
+        }
+    val p2Count = outBeams.sum()
 
-    println("[Part1] Number of Lines: ${data.size}")
+    println("[Part1] Number of beams: $p1Count")
+    // 82 (incorrect) (should have counted the splits instead of resulting beams)
+    // 1628 (correct)
+
+    println("[Part1] Number of timelines: $p2Count")
+    // 27055852018812 (correct)
 }
